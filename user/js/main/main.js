63cf77536f42a3b2f7277dc826e2f4c5969c2ec2
olseul <seul9085@naver.com>
2021-02-03 22:28:51 +0900

    메인 페이지 좌측 메뉴 버튼 클릭 시 글 띄우기(약간 수정)

--- user/js/main/main.js
+++ user/js/main/main.js
@@ -1,8 +1,4 @@
 
-//학술국 소개 클릭 시
-$("#introduceBtn").off("click").on("click", function(){
-    showintroduce();
-});
 
 //학술국 공지사항 클릭 시
 $("#noticeBtn").off("click").on("click", function(){
@@ -14,19 +10,19 @@ $("#informationBtn").off("click").on("click", function(){
     showinformation();
 });
 
-function showintroduce(){
-    //$("#menuContent").innerHTML = "학술국 소개"; 이거 왜 안돼
-    document.querySelector('#menuContent').innerHTML = "학술국 소개......어쩌구";
-}
 
 function shownotice(){
-    document.querySelector('#menuContent').innerHTML = "공지사항";
+    $("#kntnotice").css("display", "block");
+
+    $("#menuContent").empty().append("공지사항")
 }
 
 function showinformation(){
+    $("#kntinformation").css("display", "block");
+    
     var ContentHtml = "<p>학술국 정보공유<p>";
     ContentHtml += "<button id='writeBtn'>글쓰기</button>";
-    document.querySelector('#menuContent').innerHTML = ContentHtml;
+    $("#menuContent").empty().append(ContentHtml);
 }
 
 
