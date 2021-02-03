32c748ee6836a379adaf9dd7429190ab93e0d199
olseul <seul9085@naver.com>
2021-02-04 00:44:39 +0900

    공지사항 목록

--- user/js/main/main.js
+++ user/js/main/main.js
@@ -2,27 +2,43 @@
 
 //학술국 공지사항 클릭 시
 $("#noticeBtn").off("click").on("click", function(){
-    shownotice();
+    $("#kntnotice").css("display", "block");
+    $("#kntinformation").css("display", "none");
+
+    showNotice();
 });
 
 //학술국 정보공유 클릭 시 
 $("#informationBtn").off("click").on("click", function(){
+    $("#kntinformation").css("display", "block");
+    $("#kntnotice").css("display", "none");
+
     showinformation();
 });
 
+//공지사항 목록 보여주기
+function showNotice(){
 
-function shownotice(){
-    $("#kntnotice").css("display", "block");
+    var ContentHtml = "";
+    
+    ContentHtml += "<table>";
+    ContentHtml += "<thead>";
+    ContentHtml += "<th>제목</th>";
+    ContentHtml += "<th>작성자</th>";
+    ContentHtml += "<th>작성일</th>";
+    ContentHtml += "<th>조회</th>";
+    ContentHtml += "</thead>";
+    /* 나중에 db에서 받아오기 */
+    ContentHtml += "</table>";
+    $("#notice_Content").empty().append(ContentHtml);
 
-    $("#menuContent").empty().append("공지사항")
-}
+};
 
 function showinformation(){
-    $("#kntinformation").css("display", "block");
-    
-    var ContentHtml = "<p>학술국 정보공유<p>";
+    var ContentHtml = "<p>학술국 정보공유";
     ContentHtml += "<button id='writeBtn'>글쓰기</button>";
-    $("#menuContent").empty().append(ContentHtml);
-}
+    $("#information_Content").empty().append(ContentHtml);
+    
+};
 
 
