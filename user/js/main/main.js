5f915c79db525a6b20c21037bde15e3e87183cb5
olseul <seul9085@naver.com>
2021-02-04 22:51:15 +0900

    파일 수정

--- user/js/main/main.js
+++ user/js/main/main.js
@@ -3,6 +3,7 @@
 //학술국 공지사항 클릭 시
 $("#noticeBtn").off("click").on("click", function(){
     $("#kntnotice").css("display", "block");
+    $("#kntnotice_Write").css("display", "none");
     $("#kntinformation").css("display", "none");
     $("#kntStudy").css("display", "none");
 
@@ -14,17 +15,22 @@ $("#noticeBtn").off("click").on("click", function(){
 $("#informationBtn").off("click").on("click", function(){
     $("#kntinformation").css("display", "block");
     $("#kntnotice").css("display", "none");
+    $("#kntnotice_Write").css("display", "none");
     $("#kntStudy").css("display", "none");
 
-    showinformation();
+    showInformation();
 });
+
 
 //학술국 스터디 클릭 시
 $("#studyBtn").off("click").on("click", function(){
     $("#kntStudy").css("display", "block");
     $("#kntinformation").css("display", "none");
     $("#kntnotice").css("display", "none");
+    $("#kntnotice_Write").css("display", "none");
+
 });//그룹 리스트 보여주기
+
 //study a 클릭 시
 $("#studyA").on("click", function(){
     $("#studyA_Content").css("display", "block");
@@ -51,7 +57,7 @@ $("#studyC").on("click", function(){
 });
 
 //관리자가 공지사항 글쓰기 버튼 클릭 시 
-$("#writeNoticeBtn").off("click").on("click", function(){
+$("#writeNoticeBtn_Content").off("click").on("click", function(){
     $("#kntnotice").css("display", "none");
     $("#kntnotice_Write").css("display", "block");
 
@@ -59,7 +65,7 @@ $("#writeNoticeBtn").off("click").on("click", function(){
 });
 
 //공지사항 글 등록 버튼 클릭 시 
-$("#signupNoticeBtn").off("click").on("click", function(){
+$("#signupNoticeBtn_Content").off("click").on("click", function(){
     var NoticeTitle = $("#NoticeTitle").val();
     var NoticeContent = $("#NoticeContent").val();
 
@@ -72,43 +78,30 @@ $("#signupNoticeBtn").off("click").on("click", function(){
 
 //관리자 아이디인지 검사 후 버튼 생성
 function showBtn(){
-    if("세션 아이디" == "관리자 아이디"){//일단 이렇게 쓰자..
-        var btnHtml = "<button id = 'writeNoticeBtn'>글쓰기</button>";
-        $("#notice_Content").empty().append(btnHtml);
-    }
+    /*
+    if("세션 아이디" == "관리자 아이디")
+    일단 이렇게 쓰고 나중에 조건 걸고 버튼 생성해주자~
+    */
+    var btnHtml = "<button id = 'writeNoticeBtn'>글쓰기</button>";
+    $("#writeNoticeBtn_Content").empty().append(btnHtml);
+
 }
-
-//공지사항 목록 보여주기
-function showNotice(){
-
-    var ContentHtml = "";
-    
-    ContentHtml += "<table>";
-    ContentHtml +=     "<thead>";
-    ContentHtml +=         "<th>제목</th>";
-    ContentHtml +=         "<th>작성자</th>";
-    ContentHtml +=         "<th>작성일</th>";
-    ContentHtml +=         "<th>조회</th>";
-    ContentHtml +=     "</thead>";
-    /* 나중에 db에서 받아오기 */
-    ContentHtml += "</table>";
-    $("#notice_Content").empty().append(ContentHtml);
-
-};
 
 //공지사항 글 작성
 function writeNotice(){
 
     var ContentHtml = "";
+    var btnHtml = ""
 
     //일단 제목 내용만?
     ContentHtml += "<label for = 'NoticeTitle'>제목</label>";
     ContentHtml += "<input type = 'text' id = 'NoticeTitle'/>";
     ContentHtml += "<label for='NoticeContent'>내용</label>";
     ContentHtml += "<input type = 'text' id = 'NoticeContent'/>";
-    ContentHtml += "<button id = 'signupNoticeBtn'>등록</button>";
+    btnHtml += "<button id = 'signupNoticeBtn_Content'>등록</button>";
 
     $("#writeNotice_Content").empty().append(ContentHtml);
+    $("#signupNoticeBtn_Content").empty().append(btnHtml);
 
 }
 
@@ -136,6 +129,7 @@ function showStudyA_Board(){
     ContentHtml += "<button id='writeBtn'>글쓰기</button>";
     $("#studyA_Content").empty().append(ContentHtml);
 };
+
 //스터디B 게시물 목록 보여주기
 function showStudyB_Board(){
     var ContentHtml = "";
@@ -152,6 +146,7 @@ function showStudyB_Board(){
     ContentHtml += "<button id='writeBtn'>글쓰기</button>";
     $("#studyB_Content").empty().append(ContentHtml);
 };
+
 //스터디C 게시물 목록 보여주기
 function showStudyC_Board(){
     var ContentHtml = "";
