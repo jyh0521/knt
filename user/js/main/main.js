5b795db88beedb944608f41863bd923b88126ee0
joon9917 <matis3690@gmail.com>
2021-02-04 19:31:27 +0900

    main.js 스터디 그룹별 게시판

--- user/js/main/main.js
+++ user/js/main/main.js
@@ -4,6 +4,7 @@
 $("#noticeBtn").off("click").on("click", function(){
     $("#kntnotice").css("display", "block");
     $("#kntinformation").css("display", "none");
+    $("#kntStudy").css("display", "none");
 
     showBtn();
     showNotice();
@@ -13,8 +14,40 @@ $("#noticeBtn").off("click").on("click", function(){
 $("#informationBtn").off("click").on("click", function(){
     $("#kntinformation").css("display", "block");
     $("#kntnotice").css("display", "none");
+    $("#kntStudy").css("display", "none");
 
     showinformation();
+});
+
+//학술국 스터디 클릭 시
+$("#studyBtn").off("click").on("click", function(){
+    $("#kntStudy").css("display", "block");
+    $("#kntinformation").css("display", "none");
+    $("#kntnotice").css("display", "none");
+});//그룹 리스트 보여주기
+//study a 클릭 시
+$("#studyA").on("click", function(){
+    $("#studyA_Content").css("display", "block");
+    $("#studyB_Content").css("display", "none");
+    $("#studyC_Content").css("display", "none");
+
+    showStudyA_Board();
+});
+//study b 클릭 시
+$("#studyB").on("click", function(){
+    $("#studyB_Content").css("display", "block");
+    $("#studyA_Content").css("display", "none");
+    $("#studyC_Content").css("display", "none");
+
+    showStudyB_Board();
+});
+//study c 클릭 시
+$("#studyC").on("click", function(){
+    $("#studyC_Content").css("display", "block");
+    $("#studyB_Content").css("display", "none");
+    $("#studyA_Content").css("display", "none");
+
+    showStudyC_Board();
 });
 
 //관리자가 공지사항 글쓰기 버튼 클릭 시 
@@ -87,4 +120,51 @@ function showinformation(){
     
 };
 
+//스터디A 게시물 목록 보여주기
+function showStudyA_Board(){
+    var ContentHtml = "";
 
+    ContentHtml += "<table>";
+    ContentHtml +=     "<thead>";
+    ContentHtml +=         "<th>제목</th>";
+    ContentHtml +=         "<th>작성자</th>";
+    ContentHtml +=         "<th>작성일</th>";
+    ContentHtml +=         "<th>조회</th>";
+    ContentHtml +=     "</thead>";
+    /* 나중에 db에서 받아오기 */
+    ContentHtml += "</table>";
+    ContentHtml += "<button id='writeBtn'>글쓰기</button>";
+    $("#studyA_Content").empty().append(ContentHtml);
+};
+//스터디B 게시물 목록 보여주기
+function showStudyB_Board(){
+    var ContentHtml = "";
+
+    ContentHtml += "<table>";
+    ContentHtml +=     "<thead>";
+    ContentHtml +=         "<th>제목</th>";
+    ContentHtml +=         "<th>작성자</th>";
+    ContentHtml +=         "<th>작성일</th>";
+    ContentHtml +=         "<th>조회</th>";
+    ContentHtml +=     "</thead>";
+    /* 나중에 db에서 받아오기 */
+    ContentHtml += "</table>";
+    ContentHtml += "<button id='writeBtn'>글쓰기</button>";
+    $("#studyB_Content").empty().append(ContentHtml);
+};
+//스터디C 게시물 목록 보여주기
+function showStudyC_Board(){
+    var ContentHtml = "";
+
+    ContentHtml += "<table>";
+    ContentHtml +=     "<thead>";
+    ContentHtml +=         "<th>제목</th>";
+    ContentHtml +=         "<th>작성자</th>";
+    ContentHtml +=         "<th>작성일</th>";
+    ContentHtml +=         "<th>조회</th>";
+    ContentHtml +=     "</thead>";
+    /* 나중에 db에서 받아오기 */
+    ContentHtml += "</table>";
+    ContentHtml += "<button id='writeBtn'>글쓰기</button>";
+    $("#studyC_Content").empty().append(ContentHtml);
+};
