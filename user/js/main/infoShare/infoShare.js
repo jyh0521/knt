2f8d2b7a0549a7901b40129efebea5ca48e0c645
jyh <jeonyh0521@gmail.com>
2021-02-06 18:31:47 +0900

    정보공유 게시글 선택 후 글 띄우기 구현

--- user/js/main/infoShare/infoShare.js
+++ user/js/main/infoShare/infoShare.js
@@ -2,7 +2,10 @@
 ////////////////////////////////////////////////////////////////////////////// 변수 ////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var kntInfoShareList = [];
+var infoShareList = [];
+var infoShareSelectedContent = [];
+
+var infoShareSelectedListId = "";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////// 함수 ////////////////////////////////////////////////////////////////////////////
@@ -15,9 +18,8 @@ function infoShareInit(){
     $("#kntNoticeWrite").css("display", "none");
     $("#kntStudy").css("display", "none");
 
-    var contentHtml = "<p>학술국 정보공유";
-    contentHtml += "<button id='writeBtn'>글쓰기</button>";
-    $("#infoShareContent").empty().append(contentHtml);
+    $("#kntInfoShareTable").css("display", "block");
+    $("#kntInfoShareContent").css("display", "none");
 
     getInfoShareList();
 };
@@ -26,12 +28,29 @@ function infoShareInit(){
 ////////////////////////////////////////////////////////////////////////// 데이터 불러오기 //////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// 정보공유 글 리스트 불러오기
 function getInfoShareList() {
-    requestData("/knt/user/php/main/infoShare/infoShare.php").done(function(result){
-        kntInfoShareList = result;
+    requestData("/knt/user/php/main/infoShare/getInfoShareList.php").done(function(result){
+        infoShareList = result;
 
+        // 화면에 그려주기
         drawInfoShareTable();
         drawInfoShareList();
+
+        // 이벤트 등록
+        initInfoShareListEvent();
+    });
+}
+
+// 정보공유 선택된 글 내용 불러오기
+function getInfoShareContent() {
+    var param = "id=" + infoShareSelectedListId;
+
+    // 선택된 글의 id로 데이터 요청
+    requestData("/knt/user/php/main/infoShare/getInfoShareContent.php", param).done(function(result){
+        infoShareSelectedContent = result;
+
+        drawInfoShareSelectedContent();
     });
 }
 
@@ -39,6 +58,7 @@ function getInfoShareList() {
 ////////////////////////////////////////////////////////////////////////// 화면에 그리기 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// 정보공유 글 리스트 테이블 그리기
 function drawInfoShareTable() {
     var infoShareTableHtml = "";
 
@@ -56,29 +76,59 @@ function drawInfoShareTable() {
     infoShareTableHtml +=     "</tbody>";
     infoShareTableHtml += "</table>";
 
-    $("#kntInfoShare").empty().append(infoShareTableHtml);
+    $("#kntInfoShareTable").empty().append(infoShareTableHtml);
 }
 
+// 정보공유 글 리스트 그리기
 function drawInfoShareList() {
-    var kntInfoShareListHtml = "";
-    var kntInfoShareListSize = kntInfoShareList.length;
+    var infoShareListHtml = "";
+    var infoShareListSize = infoShareList.length;
 
-    for(var i = 0; i < kntInfoShareListSize; i++) {
-        kntInfoShareListHtml += "<tr>";
-        kntInfoShareListHtml +=     "<td>" + (i + 1) + "</td>";
-        kntInfoShareListHtml +=     "<td>" + kntInfoShareList[i]["BRD_TITLE"] + "</td>";
-        kntInfoShareListHtml +=     "<td>" + kntInfoShareList[i]["BRD_WRITER"] + "</td>";
-        kntInfoShareListHtml +=     "<td>" + kntInfoShareList[i]["BRD_DATE"] + "</td>";
-        kntInfoShareListHtml +=     "<td>" + kntInfoShareList[i]["BRD_HIT"] + "</td>";
-        kntInfoShareListHtml += "</tr>";
+    for(var i = 0; i < infoShareListSize; i++) {
+        infoShareListHtml += "<tr>";
+        infoShareListHtml +=     "<td>" + (i + 1) + "</td>";
+        infoShareListHtml +=     "<td class='infoShareListTitle' id='infoShareListID"+ infoShareList[i]["BRD_ID"] +"'>" + infoShareList[i]["BRD_TITLE"] + "</td>";
+        infoShareListHtml +=     "<td>" + infoShareList[i]["BRD_WRITER"] + "</td>";
+        infoShareListHtml +=     "<td>" + infoShareList[i]["BRD_DATE"] + "</td>";
+        infoShareListHtml +=     "<td>" + infoShareList[i]["BRD_HIT"] + "</td>";
+        infoShareListHtml += "</tr>";
     }
 
-    $("#kntInfoShareListTbody").empty().append(kntInfoShareListHtml);
+    $("#kntInfoShareListTbody").empty().append(infoShareListHtml);
+}
+
+function drawInfoShareSelectedContent() {
+    var infoShareSelectedContentHtml = "";
+    var infoShareSelectedContentSize = infoShareSelectedContent.length;
+
+    for(var i = 0; i < infoShareSelectedContentSize; i++) {
+        infoShareSelectedContentHtml += "<p>제목: " + infoShareSelectedContent[0]["BRD_TITLE"] + "</p>";
+        infoShareSelectedContentHtml += "<p>내용: " + infoShareSelectedContent[0]["BRD_CONTENT"]  + "</p>";
+        infoShareSelectedContentHtml += "<p>작성자: " + infoShareSelectedContent[0]["BRD_WRITER"]  + "</p>";
+        infoShareSelectedContentHtml += "<p>작성일: " + infoShareSelectedContent[0]["BRD_DATE"]  + "</p>";
+        infoShareSelectedContentHtml += "<p>조회수: " + infoShareSelectedContent[0]["BRD_HIT"]  + "</p>";
+    }
+    
+    $("#kntInfoShareTable").css("display", "none");
+    $("#kntInfoShareContent").css("display", "block");
+
+    $("#kntInfoShareContent").empty().append(infoShareSelectedContentHtml);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////// 게시판 이벤트 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 정보공유 글 리스트 이벤트 초기화
+function initInfoShareListEvent() {
+    $(".infoShareListTitle").off("click").on("click", function(){
+        // ID 자르기
+        infoShareSelectedListId = this.id.substr(15);
+
+        // 선택된 글 불러오기
+        getInfoShareContent();
+    });
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////// 기타 함수 //////////////////////////////////////////////////////////////////////////
--- user/php/main/infoShare/getInfoShareList.php
+++ user/php/main/infoShare/getInfoShareList.php
@@ -1,7 +1,9 @@
 <?php
    include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
 
-    $sql = "SELECT * FROM USR_BRD WHERE BRD_CDE = 'BRD_003'";
+    $sql = "SELECT BRD_ID, BRD_TITLE, BRD_WRITER, BRD_DATE, BRD_HIT, BRD_CDE
+              FROM USR_BRD
+             WHERE BRD_CDE = 'BRD_003' AND BRD_DISABLE = 'Y'";
 
     $result = mysql_query($sql, $connect);
 
@@ -11,7 +13,6 @@
         $tmp = array(
             'BRD_ID' => $row['BRD_ID'],
             'BRD_TITLE' => $row['BRD_TITLE'],
-            'BRD_CONTENT' => $row['BRD_CONTENT'],
             'BRD_WRITER' => $row['BRD_WRITER'],
             'BRD_DATE' => $row['BRD_DATE'],
             'BRD_HIT' => $row['BRD_HIT'],
