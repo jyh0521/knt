364323cfd38ab5c9c27b1a15f58fd682e311d39c
jyh <jeonyh0521@gmail.com>
2021-02-06 12:39:59 +0900

    정보공유 게시판 출력 구현, 디비 연결 lib 구현

---
+++ lib/php/connectDB.php
@@ -0,0 +1,5 @@
+<?
+    $connect = mysql_connect("kntdbserver.c0ekwddvziep.us-east-2.rds.amazonaws.com", "admin", "gkrtnfrnr!");
+    mysql_select_db("knt", $connect);
+    mysql_query("SET NAMES utf8");
+?>
