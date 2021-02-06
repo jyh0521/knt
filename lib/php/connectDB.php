24b2873400cc98b93979c9b0fdeaeba221dde000
jyh <jeonyh0521@gmail.com>
2021-02-06 16:13:01 +0900

    connectDB.php 수정

--- lib/php/connectDB.php
+++ lib/php/connectDB.php
@@ -1,5 +1,5 @@
 <?
-    $connect = mysql_connect("kntdbserver.c0ekwddvziep.us-east-2.rds.amazonaws.com", "admin", "gkrtnfrnr!");
+    $connect = mysql_connect("kntdbserver.crcfmeurwwst.ap-northeast-2.rds.amazonaws.com", "admin", "gkrtnfrnr!");
     mysql_select_db("knt", $connect);
     mysql_query("SET NAMES utf8");
 ?>
