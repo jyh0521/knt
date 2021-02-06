a6dd2ea674d22dee831f8f2f408477107ab4b7c0
joon9917 <matis3690@gmail.com>
2021-02-07 04:51:37 +0900

    main.js 변수명 수정

--- user/js/main/main.js
+++ user/js/main/main.js
@@ -1,6 +1,6 @@
 //학술국 공지사항 클릭 시
 $("#noticeBtn").off("click").on("click", function(){
-    $("#studyGroup").css("display", "none");
+    $("#studyGroupList").css("display", "none");
     $("#menuFuncDiv").load("noticeBrd/noticeBrd.html", function(){
         showNoticeBrd();
     });
@@ -8,7 +8,7 @@ $("#noticeBtn").off("click").on("click", function(){
 
 //학술국 정보공유 클릭 시 
 $("#infoShareBtn").off("click").on("click", function(){
-    $("#studyGroup").css("display", "none");
+    $("#studyGroupList").css("display", "none");
     $("#menuFuncDiv").load("infoShareBrd/infoShareBrd.html", function(){
         initInfoShare();
     });
@@ -16,8 +16,7 @@ $("#infoShareBtn").off("click").on("click", function(){
 
 //학술국 스터디 클릭 시
 $("#studyBtn").off("click").on("click", function(){
-    $("#studyGroup").css("display", "block");
+    $("#studyGroupList").css("display", "block");
     $("#menuFuncDiv").load("studyBrd/studyBrd.html", function(){
-        // 예준 - 스터디 게시판 보여주는 init 함수 작성할것!!
     });
 });//그룹 리스트 보여주기
