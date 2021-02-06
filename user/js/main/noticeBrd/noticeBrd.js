8fe63e50bbe405188ff2c34ecfc0738c4356d976
olseul <seul9085@naver.com>
2021-02-07 03:52:09 +0900

    noticeBrd.js 수정 / noticeBrd.html 수정 / 변수명 수정

--- user/js/main/noticeBrd/noticeBrd.js
+++ user/js/main/noticeBrd/noticeBrd.js
@@ -1,47 +1,34 @@
-////////////////////////////////////////////////////////////////////////////// 일단 main.js에서 noticeBrd.js 맨 위에 올려둠. 원하는 위치로 수정할 것 - 용후 -
-//관리자가 공지사항 글쓰기 버튼 클릭 시 
-
-
-/*$("#writeNoticeBtn").off("click").on("click", function(){
-    writeNotice();
-});
-
-//공지사항 등록 버튼 클릭 시
-$("#signUpNoticeBtn").off("click").on("click", function(){
-    setNoticeContent();
-});
-*/
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var NoticeList = [];
+var noticeBrdList = [];
 
 function showNoticeBrd(){
-    //$("#noticeContent").css("display", "block");
-    $("#noticeContent").empty().append("<p>g</p>");
-    //getNoticeList();
-    //관리자 아이디인지 검사 후 버튼 보여주기
-    //if("세션 아이디" == "관리자 아이디"){
-        /*var noticeContentHtml = "<button id = NoticeWriteBtn>글쓰기</button>";
-        $("#noticeContent").empty().append(noticeContentHtml);
+    $("#kntNoticeBrd").css("display", "block");
 
-        NoticeWriteBtn();//글쓰기 버튼 클릭 시
-        getNoticeList();*/
-    /*else{
-        getNoticeList();   관리자가 아닐경우
-    }*/
+    getNoticeBrdList();
 };
+
+//공지사항 목록 데이터 불러오기
+function getNoticeBrdList(){
+    requestData("/knt/user/php/main/noticeBrd/getNoticeList.php").done(function(result){
+        noticeBrdList = result;
+
+        showNoticeBrdTable();
+        showNoticeBrdList();
+    });
+}
+
 //공지사항 내용 데이터 저장 
-function setNoticeContent(){
-    var title = $("#signUpNoticeTitle").val();
-    var content = $("#signUpNoticeContent").val();
+function setNoticeBrdContent(){
+    var noticeBrdWriteTitle = $("#noticeBrdWriteTitle").val();
+    var noticeBrdWriteContent = $("#noticeBrdWriteContent").val();
     
-    var param = "title=" + title + "&content=" + content;
+    var param = "title=" + noticeBrdWriteTitle + "&content=" + noticeBrdWriteContent;
 
     requestData("/knt/user/php/main/noticeBrd/setNoticeContent.php", param).done(function(result){
         if(result){
             alert("등록 되었습니다.");
-            $("#Notice").css("display", "block");
-            $("#NoticeWrite").css("display", "none");
-            getNoticeList();//공지사항 목록 불러오기
+            $("#kntNoticeBrd").css("display", "block");
+            $("#kntNoticeBrdWrite").css("display", "none");
+            getNoticeBrdList();//공지사항 목록 불러오기
         }
         else{
             alert("등록 실패");
@@ -49,78 +36,73 @@ function setNoticeContent(){
     });
 }
 
-//공지사항 목록 데이터 불러오기
-function getNoticeList(){
-    requestData("/knt/user/php/main/noticeBrd/getNoticeList.php").done(function(result){
+function showNoticeBrdTable(){
 
-        NoticeList = result;
+    var kntNoticeBrdContentHtml= "";
 
-        showNoticeTable();
-        showNoticeList();
+    kntNoticeBrdContentHtml += "<table border='1'>";
+    kntNoticeBrdContentHtml +=     "<thead>";
+    kntNoticeBrdContentHtml +=         "<tr>";
+    kntNoticeBrdContentHtml +=             "<td>번호</td>";
+    kntNoticeBrdContentHtml +=             "<td>제목</td>";
+    kntNoticeBrdContentHtml +=             "<td>작성자</td>";
+    kntNoticeBrdContentHtml +=             "<td>작성일</td>";
+    kntNoticeBrdContentHtml +=             "<td>조회수</td>";
+    kntNoticeBrdContentHtml +=         "</tr>";
+    kntNoticeBrdContentHtml +=     "</thead>";
+    kntNoticeBrdContentHtml +=     "<tbody id='NoticeBrdListTbody'>";
+    kntNoticeBrdContentHtml +=     "</tbody>";
+    kntNoticeBrdContentHtml += "</table>";
+    //if(세션 아이디 == 관리자 아이디)
+    kntNoticeBrdContentHtml += "<button id = 'kntNoticeBrdWriteBtn'>글쓰기</button>"
+
+    $("#kntNoticeBrdContent").empty().append(kntNoticeBrdContentHtml);
+
+    //관리자가 글쓰기 버튼 클릭 시
+    $("#kntNoticeBrdWriteBtn").off("click").on("click", function(){
+        $("#kntNoticeBrdWrite").css("display", "block");
+        $("#kntNoticeBrd").css("display", "none");
+
+        showNoticeBrdWrite();
     });
-}
 
-function showNoticeTable(){
-
-    var noticeTableHtml= "";
-
-    noticeTableHtml += "<table border='1'>";
-    noticeTableHtml +=     "<thead>";
-    noticeTableHtml +=         "<tr>";
-    noticeTableHtml +=             "<td>번호</td>";
-    noticeTableHtml +=             "<td>제목</td>";
-    noticeTableHtml +=             "<td>작성자</td>";
-    noticeTableHtml +=             "<td>작성일</td>";
-    noticeTableHtml +=             "<td>조회수</td>";
-    noticeTableHtml +=         "</tr>";
-    noticeTableHtml +=     "</thead>";
-    noticeTableHtml +=     "<tbody id='NoticeListTbody'>";
-    noticeTableHtml +=     "</tbody>";
-    noticeTableHtml += "</table>";
-
-    $("#noticeContent").empty().append(noticeTableHtml);
 };
 
 //공지사항 목록 보여주기
-function showNoticeList() {
+function showNoticeBrdList() {
 
-    var NoticeListHtml = "";
-    var NoticeListSize = NoticeList.length;
+    var NoticeBrdListTbodyHtml = "";
+    var noticeBrdListSize = noticeBrdList.length;
 
-    for(var i = 0; i < NoticeListSize; i++) {
-        NoticeListHtml += "<tr>";
-        NoticeListHtml +=     "<td>" + (i + 1) + "</td>";
-        NoticeListHtml +=     "<td>" + NoticeList[i]["BRD_TITLE"] + "</td>";
-        NoticeListHtml +=     "<td>" + NoticeList[i]["BRD_WRITER"] + "</td>";
-        NoticeListHtml +=     "<td>" + NoticeList[i]["BRD_DATE"] + "</td>";
-        NoticeListHtml +=     "<td>" + NoticeList[i]["BRD_HIT"] + "</td>";
-        NoticeListHtml += "</tr>";
+    for(var i = 0; i < noticeBrdListSize; i++) {
+        NoticeBrdListTbodyHtml += "<tr>";
+        NoticeBrdListTbodyHtml +=     "<td>" + (i + 1) + "</td>";
+        NoticeBrdListTbodyHtml +=     "<td>" + noticeBrdList[i]["BRD_TITLE"] + "</td>";
+        NoticeBrdListTbodyHtml +=     "<td>" + noticeBrdList[i]["BRD_WRITER"] + "</td>";
+        NoticeBrdListTbodyHtml +=     "<td>" + noticeBrdList[i]["BRD_DATE"] + "</td>";
+        NoticeBrdListTbodyHtml +=     "<td>" + noticeBrdList[i]["BRD_HIT"] + "</td>";
+        NoticeBrdListTbodyHtml += "</tr>";
     }
 
-    $("#NoticeListTbody").empty().append(NoticeListHtml);
-}
-
-/*//관리자가 글쓰기 버튼 클릭 시
-function NoticeWriteBtn(){
-    $("#NoticeWriteBtn").off("click").on("click", function(){
-        writeNotice();
-    });
+    $("#NoticeBrdListTbody").empty().append(NoticeBrdListTbodyHtml);
 }
 
 //공지사항 글 작성 부분 보여주기
-function writeNotice(){
+function showNoticeBrdWrite(){
 
-    var ContentHtml = "";
-    var btnHtml = ""
-
+    var kntNoticeBrdWriteContentHtml = "";
     //일단 제목 내용만?
-    ContentHtml += "<label for = 'signUpNoticeTitle'>제목</label>";
-    ContentHtml += "<input type = 'text' id = 'signUpNoticeTitle'>";
-    ContentHtml += "<label for='signUpNoticeContent'>내용</label>";
-    ContentHtml += "<textarea id = 'signUpNoticeContent'>";
-    btnHtml += "<button>등록</button>";
+    kntNoticeBrdWriteContentHtml += "<label for = 'noticeBrdWriteTitle'>제목</label>";
+    kntNoticeBrdWriteContentHtml += "<input type = 'text' id = 'noticeBrdWriteTitle'><p>";
+    kntNoticeBrdWriteContentHtml += "<label for='noticeBrdWriteContent'>내용</label>";
+    kntNoticeBrdWriteContentHtml += "<textarea id = 'noticeBrdWriteContent'></textarea><p>";
+    kntNoticeBrdWriteContentHtml += "<button id = 'noticeBrdSignUpBtn'>등록하기</button><p>";
 
-    $("#writeNoticeContent").empty().append(ContentHtml);
-    $("#signUpNoticeBtn").empty().append(btnHtml);
+    $("#kntNoticeBrdWriteContent").empty().append(kntNoticeBrdWriteContentHtml);
 
-}*/
+    //등록하기 버튼 클릭 시
+    $("#noticeBrdSignUpBtn").off("click").on("click", function(){
+        setNoticeBrdContent();
+    });
+
+}
