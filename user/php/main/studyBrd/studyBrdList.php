9fa1e518a8dd393070ce5a3356270bad7e3ab0d1
joon9917 <matis3690@gmail.com>
2021-02-09 00:56:01 +0900

    studyBrdList.php 수정

--- user/php/main/studyBrd/studyBrdList.php
+++ user/php/main/studyBrd/studyBrdList.php
@@ -1,26 +1,27 @@
 <?php
-    include $_SERVER["DOCUMENT_ROOT"]."/lib/php/connectDB.php";
+    include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
 
     $sql = "SELECT * FROM USR_BRD
-            WHERE USR_CDE = 'STD_001'
-            AND USR_DISABLE = 'Y'";
+            WHERE BRD_CDE = 'STD_001'
+            AND BRD_DISABLE = 'Y'";
 
     $result = mysql_query($sql, $connect);
-    $row = mysql_fetch_array($result);
+   
     $value = array();
 
-    while($row) {
+    while($row = mysql_fetch_array($result)) {
         $tmp = array(
             'BRD_ID' => $row['BRD_ID'],
             'BRD_TITLE' => $row['BRD_TITLE'],
-            'BRD_CONTENT' => $row['BRD_CONTENT'],
             'BRD_WRITER' => $row['BRD_WRITER'],
             'BRD_DATE' => $row['BRD_DATE'],
-            'BRD_HIT' => $row['BRD_HIT'],
+            'BRD_HIT' => $row['BRD_HIT']
         );
 
         array_push($value, $tmp);
     }
+
+    echo json_encode($value);
 
     mysql_close($connect);
 ?>
