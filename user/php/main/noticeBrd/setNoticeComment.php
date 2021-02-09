d6eba32f2304205a86a9931babf3bacfcaea7a40
olseul <seul9085@naver.com>
2021-02-10 05:00:41 +0900

    쿠키 아이디 수정

--- user/php/main/noticeBrd/setNoticeComment.php
+++ user/php/main/noticeBrd/setNoticeComment.php
@@ -6,7 +6,7 @@
     $id = $_POST['id'];
 
     $sql = "INSERT INTO USR_BRD_CMT(CMT_CONTENT, CMT_WRITER, CMT_DATE, BRD_ID) VALUES ('$comment','ADMIN','$date','$id')";
-    //db에는 저장되는데 null이 찍힌다...............왜?
+    
     $result = mysql_query($sql, $connect);
 
     echo json_encode($result);
