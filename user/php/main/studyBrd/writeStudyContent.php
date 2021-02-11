5be26ac09a0e98724870474498b9e01df277abd4
joon9917 <matis3690@gmail.com>
2021-02-12 02:59:43 +0900

    파일 이름 변경

--- user/php/main/studyBrd/writeStudyContent.php
+++ user/php/main/studyBrd/writeStudyContent.php
@@ -1,6 +1,7 @@
 <?php
     include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
     
+    $brd = $_POST['brd'];
     $id = $_POST["id"];
     $title = $_POST["title"];
     $content = $_POST["content"];
@@ -8,7 +9,7 @@
 
     $sql = "INSERT INTO USR_BRD
                 (BRD_TITLE, BRD_WRITER, BRD_CONTENT, BRD_DATE, BRD_CDE)
-            VALUES ('$title', '$id', '$content', '$date', 'STD_001')";
+            VALUES ('$title', '$id', '$content', '$date', '$brd')";
 
     $result = mysql_query($sql, $connect);
 
