c46d642f78756a196961868168113f1c359a818b
DWH <dwhouse0@naver.com>
2021-02-15 22:08:31 +0900

    ho

--- user/php/login/signup.php
+++ user/php/login/signup.php
@@ -1,12 +1,11 @@
 <?
     include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
-    
+
     $id = $_POST['id'];
     $pw = $_POST['pw'];
     
     $sql = "INSERT INTO USR_INF(USR_ID, USR_PW) VALUES('$id','$pw')";
 
-    $connect = mysql_connect("localhost);")
     mysql_query($sql, $connect);
     mysql_close($connect);
 ?>
