cadb869a5d8c75cf372b73efcc53e1fd46da1506
joon9917 <matis3690@gmail.com>
2021-02-17 06:54:48 +0900

    getStudyCommentList.php 작성

---
+++ user/php/main/studyBrd/getStudyCommentList.php
@@ -0,0 +1,27 @@
+<?php
+    include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
+
+    $id = $_POST['brdId'];
+
+    $sql = "SELECT * FROM USR_BRD_CMT
+            WHERE BRD_ID = '$id' AND CMT_DISABLE = 'Y'";
+
+    $result = mysql_query($sql, $connect);
+
+    $value = array();
+    
+    while($row = mysql_fetch_array($result)) {
+        $tmp = array(
+            'CMT_ID' => $row['CMT_ID'],
+            'CMT_COMMENT' => $row['CMT_COMMENT'],
+            'CMT_WRITER' => $row['CMT_WRITER'],
+            'CMT_DATE' => $row['CMT_DATE']
+        );
+
+        array_push($value, $tmp);
+    }
+
+    echo json_encode($value);
+
+    mysql_close($connect);
+?>
