147061b68e57b95b5a9bed09c1721fc016a5da98
joon9917 <matis3690@gmail.com>
2021-02-17 03:30:24 +0900

    writeStudyContent.php 변수명 수정

--- user/php/main/studyBrd/writeStudyContent.php
+++ user/php/main/studyBrd/writeStudyContent.php
@@ -2,14 +2,14 @@
     include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
     
     $brd = $_POST['brd'];
-    $id = $_POST["id"];
+    $writer = $_POST["writer"];
     $title = $_POST["title"];
     $content = $_POST["content"];
     $date = $_POST["date"];
 
     $sql = "INSERT INTO USR_BRD
                 (BRD_TITLE, BRD_WRITER, BRD_CONTENT, BRD_DATE, BRD_CDE)
-            VALUES ('$title', '$id', '$content', '$date', '$brd')";
+            VALUES ('$title', '$writer', '$content', '$date', '$brd')";
 
     $result = mysql_query($sql, $connect);
 
