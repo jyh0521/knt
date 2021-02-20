02cc0d066a3d841032d2bb175a5089dbcdb6f86e
DWH <dwhouse0@naver.com>
2021-02-20 18:18:29 +0900

    회원 가입시 입력 항목 추가, DB에 입력시키는 구문 추가

--- user/php/login/idCheck.php
+++ user/php/login/idCheck.php
@@ -1,6 +1,6 @@
 <?
     //중복 여부 체크
-    include $_SERVER["DOCUMENT_ROOT"]."knt/lib/php/connectDB.php";
+    include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
 
     $id = $_POST['id'];
     $sql = "SELECT * FROM USR_INF WHERE USR_ID = '{$id}'";
@@ -9,4 +9,6 @@
     $row = mysql_num_rows($result);
 
     echo json_encode($row);
+
+    mysql_close($connect);
 ?>
--- user/php/login/signup.php
+++ user/php/login/signup.php
@@ -2,10 +2,17 @@
     include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
 
     $id = $_POST['id'];
-    $pw = $_POST['pw'];
+    $sid = $_POST['sid'];
+    $pw = $_POST[MD5('pw')];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $date = $_POST['date'];
     
-    $sql = "INSERT INTO USR_INF(USR_ID, USR_PW) VALUES('$id','$pw')";
+    $sql = "INSERT 
+            INTO USR_INF(USR_ID, USR_SID, USR_PW, USR_NAME, USR_PHONE, USER_AUTH, USR_DATE, USR_DISABLE) 
+            VALUES('$id','$sid','$pw','$name','$phone','USER','$date','Y')";
 
-    mysql_query($sql, $connect);
+    $result = mysql_query($sql, $connect);
+    echo json_encode($result);
     mysql_close($connect);
 ?>
