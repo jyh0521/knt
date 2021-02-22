057f42c82a1e6d48d0e248d55c6fbcfb2cd224df
joon9917 <matis3690@gmail.com>
2021-02-22 22:57:44 +0900

    setStudyBrdHit.php 작성

---
+++ user/php/main/studyBrd/setStudyBrdHit.php
@@ -0,0 +1,15 @@
+<?php
+    include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
+
+    $id = $_POST['brdId'];
+    
+    $sql = "UPDATE USR_BRD
+            SET BRD_HIT = BRD_HIT + 1
+            WHERE BRD_ID = '$id'";
+
+    $result = mysql_query($sql, $connect);
+
+    echo json_encode($result);
+
+    mysql_close($connect);
+?>
