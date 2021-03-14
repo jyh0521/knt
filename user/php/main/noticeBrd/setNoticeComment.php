fdcd4393a278548db8f22d5dbf6764d2bfcfaec9
olseul <seul9085@naver.com>
2021-03-14 21:30:12 +0900

    버튼 관리자에게만 보이도록 세션 비교

--- user/php/main/noticeBrd/setNoticeComment.php
+++ user/php/main/noticeBrd/setNoticeComment.php
@@ -1,11 +1,12 @@
 <?
     include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
 
+    $writer = $_POST['writer'];
     $comment = $_POST['comment'];
     $date = $_POST['date'];
     $id = $_POST['id'];
 
-    $sql = "INSERT INTO USR_BRD_CMT(CMT_CONTENT, CMT_WRITER, CMT_DATE, BRD_ID) VALUES ('$comment','ADMIN','$date','$id')";
+    $sql = "INSERT INTO USR_BRD_CMT(CMT_CONTENT, CMT_WRITER, CMT_DATE, BRD_ID) VALUES ('$comment','$writer','$date','$id')";
     
     $result = mysql_query($sql, $connect);
 
