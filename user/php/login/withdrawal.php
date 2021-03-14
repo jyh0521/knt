ed719ae7c8e4ab685b69111f4a0be1fd9eb8352e
DWH <dwhouse0@naver.com>
2021-03-14 15:51:28 +0900

    탈퇴 방식 변경

--- user/php/login/withdrawal.php
+++ user/php/login/withdrawal.php
@@ -1,9 +1,9 @@
 <?
     include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
 
-    // 데이터 한줄 삭제
     $id = $_POST['id'];
-    $sql = "DELETE FROM USR_INF WHERE USR_ID = '{$id}'";
+    $sql = "UPDATE USR_INF SET USR_DISABLE = 'N' WHERE USR_ID = '$id'";
+
     mysql_query($sql,$connect);
     mysql_close($connect);
 ?>
