d1990fc859bf5e59c62c3f858c8f866f0c1aea6b
DWH <dwhouse0@naver.com>
2021-03-14 18:41:30 +0900

    아이디 찾기 회원정보 못찾았을때 오류 메세지 제공

--- user/js/login/findID.js
+++ user/js/login/findID.js
@@ -9,16 +9,15 @@ function findmyID() {
     let usrId="";
     let param = "name=" + name + "&phone=" + phone;
     requestData("/knt/user/php/login/findID.php", param).done(function (result) {
-        if(result){
-            usrId = result[USR_ID]
-            alert("회원님의 아이디는 "+usrId+" 입니다.\n다시 로그인해주세요.")
-            location.replace('/knt/user/html/login/login.html');
-            
-        }
-        else {
+        if(result['USR_ID']===null){
             alert("회원정보가 존재하지 않습니다.");
             name.value = "";
             phone.value = "";
+        }
+        else {
+            usrId = result['USR_ID']
+            alert("회원님의 아이디는 "+usrId+" 입니다.\n다시 로그인해주세요.")
+            location.replace('/knt/user/html/login/login.html');
         }
     });
 }
