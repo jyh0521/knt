d0d1b54f107885a3fb887b27deec92cc6c20e0cf
jyh <jeonyh0521@gmail.com>
2021-05-14 15:25:51 +0900

    지원서 질문 추가 수정

--- mngr/js/main/registerForm/registerForm.js
+++ mngr/js/main/registerForm/registerForm.js
@@ -10,22 +10,23 @@ function initRegisterForm() {
 function initRegisterFormEvent() {
     // 지원서 등록 + 버튼
     document.getElementById("formContentPlus").addEventListener('click', function(){
-        // 질문 개수 제한 5개
+        // 질문 추가
         if(formContentCnt <= 5) {
-            // let formContentHtml = "";
-            // formContentHtml += "<li><input type='text' name='formContent" + formContentCnt + "' id='formContent" + formContentCnt + "' placeholder='질문을 입력해주세요.'/></li>";
-            //document.getElementById("formMainContentList").append(formContentHtml);
-            
-            let newFormContent = document.createElement("input");
-            newFormContent.setAttribute("type", "text");
-            newFormContent.setAttribute("id", "formContent" + formContentCnt);
-            newFormContent.setAttribute("name", "formContent" + formContentCnt );
-            newFormContent.setAttribute("placeholder", "질문을 입력해주세요.");
+            let newFormContentLi = document.createElement("li");
+            newFormContentLi.setAttribute("id", "formContentLi" + formContentCnt);
+            document.getElementById("formMainContentList").appendChild(newFormContentLi);
 
-            document.getElementById("formMainContentList").appendChild(newFormContent);
+            let newFormContentInput = document.createElement("input");
+            newFormContentInput.setAttribute("type", "text");
+            newFormContentInput.setAttribute("id", "formContentInput" + formContentCnt);
+            newFormContentInput.setAttribute("name", "formContentInput" + formContentCnt );
+            newFormContentInput.setAttribute("placeholder", "질문을 입력해주세요.");
+
+            document.getElementById("formContentLi" + formContentCnt).appendChild(newFormContentInput);
 
             formContentCnt++;
         }
+        // 질문 개수 제한 5개
         else {
             alert("질문은 최대 5개까지 작성할 수 있습니다.");
         }
@@ -33,6 +34,7 @@ function initRegisterFormEvent() {
 
     // 지원서 등록 - 버튼
     document.getElementById("formContentMinus").addEventListener('click', function(){
+        // 질문 삭제
         if(formContentCnt >= 1) {
             let formMainContent = document.getElementById("formMainContentList");
             formMainContent.removeChild(formMainContent.childNodes[formContentCnt - 1]);
