cfcd0b7f5b817dbcadd53a85781766a883ff6262
jyh <jeonyh0521@gmail.com>
2021-06-01 10:47:23 +0900

    지원서 제목 클릭시 지원서 질문 출력

--- mngr/js/main/formMng/formMng.js
+++ mngr/js/main/formMng/formMng.js
@@ -1,13 +1,9 @@
 // 변수
-
-
-
-
-
-
+let maxQueSize = 5;
 
 // 함수
 
+// 초기화
 function initFormMng() {
     getFormListSize();
 }
@@ -24,18 +20,29 @@ function getFormMngList(currentPage) {
     let param = 'currentPage=' + currentPage + '&dataPerPage=' + 10;
 
     requestData('/knt/mngr/php/main/formMng/getFormList.php', param).done(function(result){
-        //drawInfoShareTable();
         drawFormList(result, currentPage);
     });
 }
 
+// 지원서 질문 사항 불러오기
+function getFormContent(id) {
+    let param = 'id=' + id;
+    requestData('/knt/mngr/php/main/formMng/getFormContent.php', param).done(function(result){
+        drawFormContent(result[0]);
+    });
+}
+
+// 지원서 리스트 그리기
 function drawFormList(result, currentPage) {
+    $('#formMngTableDiv').css('display', 'block');
+    $('#formMngContentDiv').css('display', 'none');
+
     let formListHtml = '';
     let formListSize = result.length;
     let startDataIndex = currentPage * 10 - 10 + 1;
 
     for(let i = 0; i < formListSize; i++) {
-        formListHtml += '<tr id=formList' + result[i]['FORM_ID'] + '>';
+        formListHtml += '<tr id="formList' + result[i]['FORM_ID'] + '" class="formListTitle">';
         formListHtml +=     '<td>' + (startDataIndex + i) + '</td>';
         formListHtml +=     '<td>' + result[i]['FORM_TITLE'] + '</td>';
         formListHtml += '</tr>';
@@ -46,6 +53,45 @@ function drawFormList(result, currentPage) {
     initFormListEvent();
 }
 
+function drawFormContent(result) {
+    $('#formMngTableDiv').css('display', 'none');
+    $('#formMngContentDiv').css('display', 'block');
+
+    let formContentHtml = '';
+
+    formContentHtml += '<p>제목: ' + result['FORM_TITLE'] + '</p>';
+
+    for(let i = 1; i <= maxQueSize; i++) {
+        if(result['FORM_QUE' + i] != 'empty') {
+            formContentHtml += '<p>질문' + i + ': ' + result['FORM_QUE' + i] + '</p>'
+        }
+    }
+
+    $('#formMngContentDiv').empty().append(formContentHtml);
+
+    // if(infoShareSelectedContent["BRD_WRITER"] === "ADMIN") {
+    //     infoShareSelectedContentHtml += "<button id='infoShareSelectedContentDelBtn'>삭제</button>";
+    //     infoShareSelectedContentHtml += "<button id='infoShareSelectedContentUptBtn'>수정</button>";
+    // }
+
+    // infoShareSelectedContentHtml += "<button id='infoShareSelectedContentBackBtn'>뒤로</button>";
+
+    // $("#infoShareSelectedContentDiv").empty().append(infoShareSelectedContentHtml);
+
+    // initInfoShareSelectedContentEvent();
+}
+
 function initFormListEvent() {
-    
+    // 지원서 클릭 시
+    $('.formListTitle').off('click').on('click', function(){
+        let id = this.id.substr(8);
+        getFormContent(id);
+    });
+
+
+    // // 작성하기 버튼 클릭 시
+    // $("#infoShareListWriteBtn").off("click").on("click", function(){ 
+    //     writeOption = "write";       
+    //     drawInfoShareWriteContent();
+    // });
 }
