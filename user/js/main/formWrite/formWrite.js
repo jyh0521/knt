847a68e7fec848c1f2d8ec1a05b148584f856655
jyh <jeonyh0521@gmail.com>
2021-06-12 13:31:49 +0900

    지원서 제출 및 임시저장 구현

--- user/js/main/formWrite/formWrite.js
+++ user/js/main/formWrite/formWrite.js
@@ -1,10 +1,10 @@
 // 변수
-maxQueSize = 5;
+let maxQueSize = 5;
+let formContent = [];
 
 // 함수
 // 지원서 작성하기 초기화 함수
 function initFormWrite(param) {
-    //getFormWriteContent(id);
     drawFormWriteInfo(param);
     getFormWriteContent(param['id']);
     initFormWriteEvent(param);
@@ -14,7 +14,46 @@ function initFormWrite(param) {
 function getFormWriteContent(id) {
     let param = 'id=' + id;
     requestData('/knt/user/php/main/formWrite/getFormWriteContent.php', param).done(function(result){
-        drawFormWriteContent(result[0]);
+        formContent = result[0];
+        drawFormWriteContent(formContent);
+    });
+}
+
+/*
+    임시 저장된 내용이 있는지 확인하고 없으면 insert 있으면 update
+*/
+// 지원서 내용을 제출하는 함수
+function submitUserForm(content) {
+    let param = 'name=' + content['name'] + '&num=' + content['num'] + '&birth=' + content['birth'] + '&sex=' + content['sex'] + '&phone=' + content['phone'] + 
+                '&pwd=' + content['pwd'] + '&form=' + content['id'] + '&ans1=' + content['FORM_ANS1'] + '&ans2=' + content['FORM_ANS2'] + 
+                '&ans3=' + content['FORM_ANS3'] + '&ans4=' + content['FORM_ANS4'] + '&ans5=' + content['FORM_ANS5'];
+
+    requestData('/knt/user/php/main/formWrite/submitUserForm.php', param).done(function(result){
+        if(result) {
+            alert('제출되었습니다.');
+            $("#menuFuncDiv").load("noticeBrd/noticeBrd.html", function () {
+                showNoticeBrd();
+            });
+        }
+        else {
+            alert('제출실패했습니다.');
+        }
+    });
+}
+
+// 지원서 내용을 임시저장 하는 함수
+function saveUserForm(content) {
+    let param = 'name=' + content['name'] + '&num=' + content['num'] + '&birth=' + content['birth'] + '&sex=' + content['sex'] + '&phone=' + content['phone'] + 
+                '&pwd=' + content['pwd'] + '&form=' + content['id'] + '&ans1=' + content['FORM_ANS1'] + '&ans2=' + content['FORM_ANS2'] + 
+                '&ans3=' + content['FORM_ANS3'] + '&ans4=' + content['FORM_ANS4'] + '&ans5=' + content['FORM_ANS5'];
+
+    requestData('/knt/user/php/main/formWrite/saveUserForm.php', param).done(function(result){
+        if(result) {
+            alert('저장되었습니다.');
+        }
+        else {
+            alert('저장 실패했습니다.');
+        }
     });
 }
 
@@ -36,7 +75,7 @@ function drawFormWriteContent(param) {
         if(param['FORM_QUE' + i] != 'empty') {
             formWriteContentHtml += '<li>';
             formWriteContentHtml += '<p>' + i + '. ' + param['FORM_QUE' + i] + '</p>';
-            formWriteContentHtml += '<textarea id="formQueTextArea' + i + '"></textarea>';
+            formWriteContentHtml += '<textarea id="formAnsTextArea' + i + '"></textarea>';
             formWriteContentHtml += '</li>';
         }
     }
@@ -49,12 +88,56 @@ function drawFormWriteContent(param) {
 function initFormWriteEvent(param) {
     // 임시저장 버튼 클릭 시
     $('#formWriteTempSaveBtn').off('click').on('click', function() {
-        
+        param['birth'] = $('#userBirth').val();
+        param['sex'] = $('#userSex').val();
+        param['phone'] = $('#userPhone').val();
+
+        for(let i = 1; i <= maxQueSize; i++) {
+            if(formContent['FORM_QUE' + i] != 'empty') {
+                param['FORM_ANS' + i] = $('#formAnsTextArea' + i).val();
+            }
+            else {
+                param['FORM_ANS' + i] = 'empty';
+            }
+        }
+
+        if(confirm('지원서를 임시 저장하시겠습니까? 반드시 제출하셔야 지원이 완료됩니다.')) {
+            saveUserForm(param);
+        }
+        else {
+            alert('취소되었습니다.');
+        }
     });
 
     // 제출하기 버튼 클릭 시
     $('#formWriteSubmitBtn').off('click').on('click', function() {
+        param['birth'] = $('#userBirth').val();
+        param['sex'] = $('#userSex').val();
+        param['phone'] = $('#userPhone').val();
+        
+        if(formContentValidationCheck(param)) {
+            // 지원서 질문의 답변이 다 쓰였는지 검사
+            for(let i = 1; i <= maxQueSize; i++) {
+                if(formContent['FORM_QUE' + i] != 'empty') {
+                    if($('#formAnsTextArea' + i).val().trim() === '') {
+                        alert('질문' + i + '를 작성해주세요.');
+                        return false;
+                    }
+                    else
+                        param['FORM_ANS' + i] = $('#formAnsTextArea' + i).val();
+                }
+                else {
+                    param['FORM_ANS' + i] = 'empty';
+                }
+            }
 
+            if(confirm('지원서를 제출하시겠습니까? 제출 후에는 수정이 불가능합니다.')) {
+                submitUserForm(param);
+            }
+            else {
+                alert('취소되었습니다.');
+            }
+        }
     });
 
     // 취소 버튼 클릭 시
@@ -68,13 +151,24 @@ function initFormWriteEvent(param) {
     });
 }
 
+// 지원서 작성 내용 유효성 검사
+function formContentValidationCheck(param) {
+    if(param['birth'].trim() === '') {
+        alert('생년월일을 입력하세요.');
+        return false;
+    }
+    else if(param['sex'].trim() === '') {
+        alert('성별을 입력하세요.');
+        return false;
+    }   
+    else if(param['phone'].trim() === '') {
+        alert('연락처를 입력하세요.');
+        return false;
+    }
 
+    return true;
+}
 
-
-
-       // param['birth'] = $('#userBirth').val();
-        // param['sex'] = $('#userSex').val();
-        // param['phone'] = $('#userPhone').val();
 
 
 
---
+++ user/php/main/formWrite/saveUserForm.php
@@ -0,0 +1,27 @@
+<?
+    include $_SERVER["DOCUMENT_ROOT"]."/knt/lib/php/connectDB.php";
+
+    $name = $_POST['name'];
+    $num = $_POST['num'];
+    $birth = $_POST['birth'];
+    $sex = $_POST['sex'];
+    $phone = $_POST['phone'];
+    $pwd = $_POST['pwd'];
+    $form = $_POST['form'];
+    $ans1 = $_POST['ans1'];
+    $ans2 = $_POST['ans2'];
+    $ans3 = $_POST['ans3'];
+    $ans4 = $_POST['ans4'];
+    $ans5 = $_POST['ans5'];
+
+    $sql = "INSERT 
+              INTO USR_SUB_FORM (SUB_FORM_NAME, SUB_FORM_NUM, SUB_FORM_BIRTH, SUB_FORM_SEX, SUB_FORM_PHONE, SUB_FORM_PWD, FORM_ID, 
+                                 SUB_FORM_ANS1, SUB_FORM_ANS2, SUB_FORM_ANS3, SUB_FORM_ANS4, SUB_FORM_ANS5, SUB_FORM_ISSUBMIT) 
+            VALUES ('$name', '$num', '$birth', '$sex', '$phone', '$pwd', '$form', '$ans1', '$ans2', '$ans3', '$ans4', '$ans5', 'N')";
+
+    $result = mysql_query($sql, $connect);
+
+    echo json_encode($result);
+
+    mysql_close($connect);
+?>
