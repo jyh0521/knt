fdf93082462d7110f42943af1677059537825dad
jyh <jeonyh0521@gmail.com>
2021-06-18 17:56:09 +0900

    저장된 지원서 수정 페이지 값 불러오는것 까지만 구현

--- user/js/main/myForm/myForm.js
+++ user/js/main/myForm/myForm.js
@@ -18,16 +18,30 @@ function getSavedMyForm(name, num, pwd) {
     });
 }
 
-function getMySelectedForm(id) {
+function getMySelectedForm(id, mode) {
     let param = 'id=' + id;
 
     requestData('/knt/user/php/main/myForm/getMySelectedForm.php', param).done(function(myForm){
         param = 'id=' + myForm[0]['FORM_ID'];
 
         requestData('/knt/user/php/main/formWrite/getFormWriteContent.php', param).done(function(formContent){
-            $('#menuFuncDiv').load('myForm/myFormChk.html', function(){
-                drawMySelectedForm(myForm[0], formContent[0]);
-            });
+            if(mode === 'check') {
+                $('#menuFuncDiv').load('myForm/myFormChk.html', function(){
+                    drawMySelectedForm(myForm[0], formContent[0]);
+                });
+            }
+            else if(mode ==='update') {
+                $('#menuFuncDiv').load('formWrite/formWrite.html', function(){
+                    drawFormWriteContent(formContent[0]);
+                    setMyFormAnswer(myForm[0], formContent[0]);
+
+                    /*
+                        TODO
+                        1. 이벤트 모드 2개로 설정해서 함수 만들기 or 새로 함수 만들기
+                    */
+                    initFormWriteEvent()
+                });
+            }
         });
     });
 }
@@ -96,6 +110,20 @@ function drawMySelectedForm(myForm, formContent) {
     $('#myFormChkQusDiv').empty().append(mySelectedFormQusHtml);
 }
 
+function setMyFormAnswer(myForm, formContent) {
+    $('#subUserName').empty().append('이름: ' +  myForm['SUB_FORM_NAME']);
+    $('#subUserNum').empty().append('학번: ' + myForm['SUB_FORM_NUM']);
+    $('#userBirth').val(myForm['SUB_FORM_BIRTH']);
+    $('#userSex').val(myForm['SUB_FORM_SEX']);
+    $('#userPhone').val(myForm['SUB_FORM_PHONE']);
+
+    for(let i = 1; i <= 5; i++) {
+        if(formContent['FORM_QUE' + i] != 'empty') {
+            $('#formAnsTextArea' + i).val(myForm['SUB_FORM_ANS' + i]);
+        }
+    }
+}
+
 function initMyFormBtnEvent() {
     // 조회하기 버튼 클릭 시
     $('#myFormFindBtn').off('click').on('click', function() {
@@ -131,7 +159,7 @@ function initMyFormListEvent() {
         // 조회 버튼
         $('.myFormListChkBtn').off('click').on('click', function(){
             if(confirm('해당 지원서를 조회하시겠습니까?')) {
-                getMySelectedForm(selectedId);
+                getMySelectedForm(selectedId, 'check');
             }
             else {
                 alert('취소되었습니다.');
@@ -140,7 +168,12 @@ function initMyFormListEvent() {
 
         // 수정 버튼
         $('.myFormListUptBtn').off('click').on('click', function(){
-            
+            if(confirm('해당 지원서를 수정하시겠습니까?')) {
+                getMySelectedForm(selectedId, 'update');
+            }
+            else {
+                alert('취소되었습니다.');
+            }
         });
 
         // 삭제 버튼
