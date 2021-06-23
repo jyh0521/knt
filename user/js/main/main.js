64ddc7b4288ef05a1c7cc3fa6e8a0edb7e4d2bee
jyh <jeonyh0521@gmail.com>
2021-06-23 16:31:55 +0900

    페이지가 로드되고 공지사항이 먼저 뜨게 수정

--- user/js/main/main.js
+++ user/js/main/main.js
@@ -1,6 +1,9 @@
 //ui 드롭다운 안먹힐때
 window.onload = function () {
     $('.ui .dropdown').dropdown();
+    
+    // 학술국 공지사항 먼저 실행
+    noticeBrd.initNoticeBrd();
 };
 
 //학술국 공지사항 클릭 시
@@ -13,11 +16,16 @@ $("#noticeBtn").off("click").on("click", function () {
 
 // 내 지원서 클릭 시
 $('#myFormBtn').off('click').on('click', function(){
-    // var state = {'page_id' : 'main.html'};
-    // history.pushState(state, null, 'myForm.html');
+    var state = {'page_id' : noticeBrd.initNoticeBrd() };
+    history.pushState(state, null, 'myForm.html');
 
     myForm.initMyForm();
 });
+
+/*
+    TODO
+    1. 학술국 소개 부분 작성, 일단 공지사항이 맨 처음 보이게 해둠
+*/
 
 // window.onpopstate = function(event) {
 //     loadStateContent(event.state);
