64b84000124b486a7d1c20a007104f9aa29de5d8
jyh <jeonyh0521@gmail.com>
2021-06-25 15:45:00 +0900

    공지사항 검색기능 오류 수정

--- user/js/main/noticeBrd/noticeBrd.js
+++ user/js/main/noticeBrd/noticeBrd.js
@@ -156,7 +156,7 @@ let noticeBrd = (function() {
             }
             else{
                 searchOrAll = "search"
-                showNoticeBrd();
+                getNoticeSearchListCount();
             }
         });
     };
