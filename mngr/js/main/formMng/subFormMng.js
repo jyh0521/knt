ed6cfc712728a8f8e8e8dbf78750e8ed1104932d
jyh <jeonyh0521@gmail.com>
2021-06-28 15:56:19 +0900

    지원서 조회 기능 구현, css 적용

--- mngr/js/main/formMng/subFormMng.js
+++ mngr/js/main/formMng/subFormMng.js
@@ -5,20 +5,35 @@ let subFormMng = (function(){
         });
     }
 
-    // 목록 전체 데이터 수 불러오기 
+    // 지원서 목록 전체 데이터 수 불러오기 
     function getSubFormListCount(){
         requestData('/knt/mngr/php/main/formMng/subFormMng/getSubFormListCount.php').done(function(result){
             DrawPaging(result['COUNT'], 10, 1, 'subFormMngPagingDiv',  getSubFormList);
         });
     }
 
-    // 게시판 목록 불러오기(id, 제목, 작성자, 작성일, 삭제여부)
+    // 지원서 목록 불러오기
     function getSubFormList(currentPage){
         let startrow = (currentPage - 1) * 10;
         let param = "startrow=" + startrow;
 
         requestData('/knt/mngr/php/main/formMng/subFormMng/getSubFormList.php', param).done(function(result){
             drawSubFormList(currentPage, result);
+        });
+    }
+
+    // 지원서 내용 불러오기
+    function getSubFormContent(id){
+        let param = "id=" + id;
+
+        requestData('/knt/mngr/php/main/formMng/subFormMng/getSubFormContent.php', param).done(function(subForm){
+            param = "id=" + subForm[0]['FORM_ID'];
+
+            requestData('/knt/mngr/php/main/formMng/subFormMng/getFormContent.php', param).done(function(formContent){
+                $('#menuFuncDiv').load('formMng/subFormMng/subFormContent.html', function(){
+                    drawSubFormContent(subForm[0], formContent[0]);
+                });
+            });
         });
     }
 
@@ -37,21 +52,48 @@ let subFormMng = (function(){
 
         $("#subFormListTbody").empty().append(subFormListHtml);
 
-        // let noticeBrdMngListTbodyHtml = "";
-        // let noticeBrdMngListSize = noticeBrdMngList.length;
-        // let startDataIndex = currentPage * 10 - 10 + 1;
-    
-        // for(let i = 0; i < noticeBrdMngListSize; i++) {
-        //     noticeBrdMngListTbodyHtml += '<tr class="noticeBrdListTr" id="noticeBrdListId' + noticeBrdMngList[i]['BRD_ID'] + '">';
-        //     noticeBrdMngListTbodyHtml +=     '<td class="noticeBrdListNum">' + (startDataIndex + i) + '</td>';
-        //     noticeBrdMngListTbodyHtml +=     '<td class="noticeBrdListTitle">' + noticeBrdMngList[i]['BRD_TITLE'] + '</td>';
-        //     noticeBrdMngListTbodyHtml +=     "<td>" + cmpTimeStamp(noticeBrdMngList[i]["BRD_DATE"]) + "</td>";
-        //     noticeBrdMngListTbodyHtml += "</tr>";
-        // }
-    
-        // $("#noticeBrdMngListTbody").empty().append(noticeBrdMngListTbodyHtml);
-    
-        // initNoticeBrdMngListEvent();
+        initSubFormListEvent();
+    }
+
+    function drawSubFormContent(subForm, formContent) {
+
+        let subFormTitleHtml = '';
+
+        subFormTitleHtml += '<h4 class="title">' + formContent['FORM_TITLE'] + '</h4>';
+        
+        $('#subFormTitle').empty().append(subFormTitleHtml);
+
+        let subFormInfoHtml = '';
+
+        subFormInfoHtml += '<h3 class="infoTitle">기본 인적사항</h3>';
+        subFormInfoHtml += '<p class="info">' + subForm['SUB_FORM_NAME'] + '</p>';
+        subFormInfoHtml += '<p class="info">' + subForm['SUB_FORM_NUM'] + '</p>';
+        subFormInfoHtml += '<p class="info">' + subForm['SUB_FORM_BIRTH'] + '</p>';
+        subFormInfoHtml += '<p class="info">' + subForm['SUB_FORM_SEX'] + '</p>';
+
+        $('#subFormInfoDiv').empty().append(subFormInfoHtml);
+
+        let subFormQusHtml = '';
+
+        for(let i = 1; i <= 5; i++) {
+            if(formContent['FORM_QUE' + i] != 'empty') {
+                subFormQusHtml += '<div class="qusAns">'
+                subFormQusHtml += '<p class="qus">' + i +'. ' + formContent['FORM_QUE' + i] + '</p>';
+                subFormQusHtml += '<p>' + subForm['SUB_FORM_ANS' + i] + '</p>';
+                subFormQusHtml += '</div>'
+            } 
+        }
+
+        $('#subFormQusSubDiv').empty().append(subFormQusHtml);
+    }
+
+    // 제출된 지원서 목록 이벤트
+    function initSubFormListEvent() {
+        // 항목 선택 시
+        $('.subFormListTr').off('click').on('click', function(){
+            let id = this.id.substr(13);
+            getSubFormContent(id);
+        });
     }
 
     return {
